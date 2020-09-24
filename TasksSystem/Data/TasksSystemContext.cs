4150df09074d96b12cc6af33f2a8ab7ab0fc2954
Koroleva Masha <markor2000@inbox.ru>
2020-09-24 12:07:37 +0300

    add

---
+++ TasksSystem/Data/TasksSystemContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TasksSystem.Models;
+
+namespace TasksSystem.Data
+{
+    public class TasksSystemContext : DbContext
+    {
+        public TasksSystemContext(DbContextOptions<TasksSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Task> Task { get; set; }
+    }
+}
---
+++ TasksSystem/Models/Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasksSystem.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreationDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DeadlineDate { get; set; }
+
+        //public Status status { get; set; }
+
+        public int UserId { get; set; }
+        
+        public int NumberOfDays { get; set; }
+
+        public string Text { get; set; }
+    }
+}
